b779c8b027abea938df4ace79dc88e807ef22928
Chirag Kantharia <chirag.kantharia@gmail.com>
2009-01-01 05:33:59 +0530

    Add search service

--- Makefile
+++ Makefile
@@ -1,15 +1,28 @@
-NAME := ingest
+INGEST := ingest
+SEARCH := search
 
-.PHONY: all build test clean
+.PHONY: all build test run-ingest up clean
 
 all: build
 
-build:
-	docker build -t $(NAME) .
+build: build-ingest build-search
+
+build-ingest:
+	docker build -f Dockerfile.ingest -t $(INGEST) .
+
+build-search:
+	docker build -f Dockerfile.search -t $(SEARCH) .
+
+run-ingest:
+	docker run --rm --network=host --volume $(PWD):$(PWD) \
+		--volume /tmp:/tmp --workdir $(PWD) --env DROPBOX_TOKEN $(INGEST)
 
 test: build
-	docker run --rm --network=host --volume $(PWD):$(PWD) \
-		--volume /tmp:/tmp --workdir $(PWD) --env DROPBOX_TOKEN $(NAME)
+	docker-compose up -d
+
+up: build
+	docker-compose up -d
 
 clean:
-	-docker rmi $(NAME)
+	-docker-compose down -v
+	-docker rmi $(INGEST) $(SEARCH)
---
+++ search.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import json
+import logging
+from flask import Flask, request
+from elasticsearch import Elasticsearch
+
+ES_HOST = 'localhost:9200'
+ES_INDEX = 'docsearchdemo'
+
+app = Flask(__name__)
+es_client = Elasticsearch(hosts=[ES_HOST])
+
+
+@app.route('/')
+def index():
+    """ Index routine """
+    return json.dumps({})
+
+
+@app.route('/search')
+def search():
+    """ Search routine """
+    search_term = request.args.get('q')
+    query_body = {"query": {"match": {"text": search_term}}}
+    es_response = es_client.search(index=ES_INDEX, body=query_body)
+    try:
+        count = es_response['hits']['total']['value']
+        hits = [hit['_id'] for hit in es_response['hits']['hits']]
+        response = {'items': count, 'paths': hits}
+        return response
+    except Exception as error:
+        logging.warning('%s: %s', type(error), error)
+    return es_response
+
+
+logging.basicConfig(level=logging.DEBUG, format='%(levelname)s %(message)s')
+app.run(host='0.0.0.0', port=9000)
